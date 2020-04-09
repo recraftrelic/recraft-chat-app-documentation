c406c82f0e44e0cf763dd399888052f4f719ce0a
Yogesh Kapoor <yashkapoor8710@gmail.com>
2020-04-09 15:13:08 +0530

    correct-url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'Recraft Chat App', // Title for your website.
   tagline: 'A website for docs',
-  url: 'https://documentation.recraftrelic.com/', // Your website URL
-  baseUrl: '/chat/', // Base URL for your project */
+  url: 'https://documentation.recraftrelic.com/chatapp/', // Your website URL
+  baseUrl: '/chatapp/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'chat-app',
+  projectName: 'chatapp',
   organizationName: 'Recraft',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
