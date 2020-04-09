d5e9cbb950522fd4b784580a7ca36daf72f6f7f8
Yogesh Kapoor <yashkapoor8710@gmail.com>
2020-04-09 12:15:57 +0530

    correct-url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Recraft Chat App', // Title for your website.
   tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://documentation.recraftrelic.com/chat/', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
